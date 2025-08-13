fd0aa06f05f8091cb99c5f088138753f2f6499b2
Jason Figueroa <193769940+JFigueroa1113@users.noreply.github.com>
2025-08-13 11:12:50 -0400

    Added conditional, mark as complete, disable submit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,41 @@
 import { useState } from 'react';
 import TodoForm from './TodoForm';
 import TodoList from './TodoList';
-import './App.css'
-
-
+import './App.css';
 
 function App() {
   const [todoList, setTodoList] = useState([
-    { id: 1, title: 'Learn React' },
-    { id: 2, title: 'Build a Todo App' }
+    { id: 1, title: 'Learn React', isCompleted: false },
+    { id: 2, title: 'Build a Todo App', isCompleted: false }
   ]);
 
- function addTodo(title) {
+  function addTodo(title) {
     const newTodo = {
       id: Date.now(),
       title,
+      isCompleted: false
     };
 
     setTodoList([...todoList, newTodo]);
-    console.log("Updated list:", [...todoList, newTodo]);
   }
 
+  function completeTodo(id) {
+    const updatedTodos = todoList.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, isCompleted: true };
+      }
+      return todo;
+    });
+    setTodoList(updatedTodos);
+  }
 
   return (
-      <main>
-        <h1>My Todos</h1>
-        <TodoForm onAddTodo={addTodo} />
-        
-        <TodoList todoList={todoList} />
-      </main> 
+    <main>
+      <h1>My Todos</h1>
+      <TodoForm onAddTodo={addTodo} />
+      <TodoList todoList={todoList} onCompleteTodo={completeTodo} />
+    </main>
   );
 }
 
-export default App
+export default App;
